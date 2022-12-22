8bc3822b519ca095dd89fcdb6f392ec0a65d71b9
Luciano Santos <contato@lucianosantos.dev>
2022-12-22 14:10:03 -0300

    Setup navbar

--- app/src/main/java/dev/lucianosantos/exerciseplanner/MainActivity.kt
+++ app/src/main/java/dev/lucianosantos/exerciseplanner/MainActivity.kt
@@ -2,10 +2,35 @@ package dev.lucianosantos.exerciseplanner
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import dev.lucianosantos.exerciseplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setupAppBarNavigation()
+    }
+
+
+    private fun setupAppBarNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
