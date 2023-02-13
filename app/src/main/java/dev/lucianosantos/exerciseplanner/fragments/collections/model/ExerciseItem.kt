64adc5027dcb68e86283874a48d14517ff8fb7c5
Luciano Santos <contato@lucianosantos.dev>
2023-02-13 14:24:48 -0300

    Add more ui state

--- app/src/main/java/dev/lucianosantos/exerciseplanner/fragments/collections/model/ExerciseItem.kt
+++ app/src/main/java/dev/lucianosantos/exerciseplanner/fragments/collections/model/ExerciseItem.kt
@@ -1,5 +1,7 @@
 package dev.lucianosantos.exerciseplanner.fragments.collections.model
 
+import dev.lucianosantos.exerciseplanner.core.model.ExerciseDomain
+
 data class ExerciseItem (
     val id: String,
     val name: String,
